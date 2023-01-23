7f4a75eedb2126eb42416e44ffc7483f36d9c544
Jonathan Dahan <dahanj95@gmail.com>
2023-01-24 00:35:31 +0200

    Initial commit

---
+++ PyFormat/Models/Header.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyFormat.Models
+{
+    public class Header
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public Header(string line)
+        {
+            string[] split = line.Split(':');
+            Name = split[0];
+            Value = split[1].TrimStart();
+        }
+
+        public override string ToString()
+        {
+            return $"'{Name}':'{Value}',";
+        }
+    }
+}
---
+++ PyFormat/Program.cs
@@ -0,0 +1,14 @@
+﻿using PyFormat.Models;
+using System.Text;
+
+string[] lines = File.ReadAllLines(".\\headers.txt");
+
+StringBuilder builder = new();
+
+foreach(string line in lines)
+{
+    Header header = new(line);
+    builder.AppendLine(header.ToString());
+}
+
+File.WriteAllText(".\\headers_fixed.txt", builder.ToString());
